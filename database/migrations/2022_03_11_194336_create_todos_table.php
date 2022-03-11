ed92407efd6f1fc7f8551fcac89103a5c22416cb
Crxwler <loco7139@gmail.com>
2022-03-11 13:45:10 -0600

    add: todos table

--- database/migrations/2022_03_11_194336_create_todos_table.php
+++ database/migrations/2022_03_11_194336_create_todos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTableTodo extends Migration
+class CreateTodosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class CreateTableTodo extends Migration
      */
     public function up()
     {
-        Schema::create('table_todo', function (Blueprint $table) {
+        Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->string('todo');
+            $table->integer('done');
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ class CreateTableTodo extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_todo');
+        Schema::dropIfExists('todos');
     }
 }
